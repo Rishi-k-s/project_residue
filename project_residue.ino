2fc49029119be78bbc7db35097e7fa7f7145001a
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-11-15 03:16:57 +0530

    added constants

--- project_residue.ino
+++ project_residue.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h> // TX RX software library for bluetooth
+#include <Servo.h> // servo library 
 
+Servo dispenseServo;
+
+int bluetoothTx = 10; // bluetooth tx to 10 pin
+int bluetoothRx = 11; // bluetooth rx to 11 pin
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+const int power_led = 9;
+const int connection_led = 8;
+const int success_led = 7;
+const int put_trash_push_button = 6;
+
+int is_connected = 0;
+
+void setup() {
+  dispenseServo.attach(6);
+  Serial.begin(9600);// serial monitor stuff
+  bluetooth.begin(9600); // Bluetooth connection
+
+  pinMode(power_led, OUTPUT);
+  pinMode(connection_led, OUTPUT);
+  pinMode(success_led, OUTPUT);
+
+  pinMode(put_trash_push_button, INPUT);
+
+  digitalWrite(power_led, HIGH); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(bluetooth.available()){
+    Serial.write("")
+  }
 
 }
