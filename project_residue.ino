38fb8ab912692bf7526f43790f3b7830490a43ac
Rishi-k-s <rishikrishna.sr@gmail.com>
2022-11-15 15:22:13 +0530

    added files

--- project_residue.ino
+++ project_residue.ino
@@ -11,10 +11,12 @@ SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 const int power_led = 9;
 const int connection_led = 8;
 const int success_led = 7;
-const int put_trash_push_button = 6;
+const int put_trash_push_button = 2;
 
 int is_connected = 0;
 int buttonState = 0;          //variables for reading the pushbutton status
+
+String incoming_Val;
 
 void setup() {
   dispenseServo.attach(6);
@@ -27,28 +29,37 @@ void setup() {
   pinMode(success_led, OUTPUT);
 
   pinMode(put_trash_push_button, INPUT);
+  digitalWrite(put_trash_push_button, HIGH);
 
   digitalWrite(power_led, HIGH); 
 }
 
 void loop() {
-  buttonState = digitalRead(put_trash_push_button); //read the state of the pushbutton value
-
-  if(bluetooth.available() >0 && is_connected<0){
-    
-    Serial.println("Connection Etablished");
-    digitalWrite(connection_led, HIGH);
-    is_connected = 1;
-  }
-  if(is_connected == 1){
-    if(buttonState == HIGH){
+  if (bluetooth.available()){
+    Serial.println("COnnecteddd!");
+    incoming_Val = bluetooth.readString();
+    Serial.println(incoming_Val);
+    if(incoming_Val == "1"){
+      Serial.println("IThaa poiii");
       dispenseServo.write(180);
-      digitalWrite(success_led, HIGH); 
-      bluetooth.write("+1");
+      digitalWrite(success_led, HIGH);
       delay(1500);
-      digitalWrite(success_led, LOW); 
       dispenseServo.write(90);
+      digitalWrite(success_led, LOW); 
     }
   }
+  // buttonState = digitalRead(put_trash_push_button); //read the state of the pushbutton value
+  //   if (buttonState == LOW){
+  //     Serial.println("Button pressed");
+  //     dispenseServo.write(180);
+  //     digitalWrite(success_led, HIGH); 
+  //     bluetooth.write("+1");
+  //     delay(1500);
+  //     digitalWrite(success_led, LOW); 
+  //     dispenseServo.write(90);
+  //   }
+      
+    
+  
 
 }
